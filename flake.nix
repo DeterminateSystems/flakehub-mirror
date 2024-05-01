fd56c5a7dcb968ac6f2f757eda09516560c3aebf
Luc Perkins <lucperkins@gmail.com>
2024-05-01 15:03:35 -0300

    Initial JS setup

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "flakehub-mirror";
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            jq
+            shellcheck
+            nodejs_latest
+            nixpkgs-fmt
+            nodePackages_latest.pnpm
+            nodePackages_latest.typescript-language-server
+          ];
+        };
+      });
+    };
+}
